e34badda3dcfb6dde5663edc6a5bc33125b26718
Jabes Bueno <jabesbueno@hotmail.com>
2020-06-03 17:03:34 -0300

    Backend parte 2

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,44 +1,10 @@
 import express from 'express';
-//import { request } from 'http';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const users = [
-    'Jabes',
-    'Bueno',
-    'Livramento',
-    'teste'
-];
-
-app.get('/users',(request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    //JSON
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id',(request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users',(request, response) => {
-    const data = request.body;
-    
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    //JSON
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
