c48676a747c2e347f53f76099e677825d05be590
Jabes Bueno <jabesbueno@hotmail.com>
2020-06-02 22:36:42 -0300

    Backend parte 1

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,17 +1,44 @@
 import express from 'express';
+//import { request } from 'http';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Jabes',
+    'Bueno',
+    'Livramento',
+    'teste'
+];
+
 app.get('/users',(request, response) => {
-    console.log("Listagem de usuários");
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
     //JSON
-    response.json([
-        'Jabes',
-        'Bueno',
-        'Livramento',
-        'teste'
-    ]);
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id',(request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users',(request, response) => {
+    const data = request.body;
+    
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    //JSON
+    return response.json(user);
 });
 
 app.listen(3333);
